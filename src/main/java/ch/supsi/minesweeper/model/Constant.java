6b7f973726cb260df5ff7e507eadfc642ea68baa
Luca Mazza <luca.mazza@student.supsi.ch>
2025-05-04 22:12:52 +0200

    refactor: added constants file to manage program constants

---
+++ src/main/java/ch/supsi/minesweeper/model/Constant.java
@@ -0,0 +1,14 @@
+package ch.supsi.minesweeper.model;
+
+public final class Constant {
+    // GRID SIZING /////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static final int GRID_HEIGHT = 9;
+    public static final int GRID_WIDTH  = 9;
+    public static final int TILE_COUNT  = GRID_HEIGHT * GRID_WIDTH;
+    // BOMB INVARIANTS /////////////////////////////////////////////////////////////////////////////////////////////////
+    public static final int MIN_BOMBS       = 1;
+    public static final int MAX_BOMBS       = TILE_COUNT - 1;
+    public static final int DEFAULT_BOMBS   = 10;
+
+    private Constant() {}
+}
